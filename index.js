a921a77436df1b1e4d194b0356c773c2c90c81f0
Stephanie Nemeth <traumverloren@gmail.com>
2021-10-16 22:07:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const aedes = require("aedes")();
+const mqtt = require("net").createServer(aedes.handle);
+const httpServer = require("http").createServer(app);
+const ws = require("websocket-stream");
+const mqttPort = 1883;
+const appPort = 8080;
+
+mqtt.listen(mqttPort, function() {
+  console.log("mqtt server listening on port", mqttPort);
+});
+
+ws.createServer(
+  {
+    server: httpServer
+  },
+  aedes.handle
+);
+
+httpServer.listen(appPort, function() {
+  console.log("server listening on port", appPort);
+});
+
+aedes.on("clientError", function(client, err) {
+  console.log("client error", client.id, err.message, err.stack);
+});
+
+aedes.on("publish", function(packet, client) {
+  if (client) {
+    console.log("message from client", client.id);
+  }
+});
+
+aedes.on("subscribe", function(subscriptions, client) {
+  if (client) {
+    console.log("subscribe from client", subscriptions, client.id);
+  }
+});
+
+aedes.on("client", function(client) {
+  console.log("new client", client.id);
+});
+
+app.get("/", (req, res) => {
+  res.send('server is working!');
+});
