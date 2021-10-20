c60e4f6898214377db84f515607e9be286706b96
Stephanie Nemeth <traumverloren@gmail.com>
2021-10-20 22:00:02 +0200

    add twitter api call but doesn't update count :frowning:

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -8,6 +8,15 @@ const httpServer = require("http").createServer(app);
 const ws = require("websocket-stream");
 const mqttPort = 1883;
 const appPort = 8080;
+
+const client = require('twitter-api-client');
+
+const twitterClient = new client.TwitterClient({
+  apiKey: process.env.CONSUMER_KEY,
+  apiSecret: process.env.CONSUMER_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessTokenSecret: process.env.ACCESS_SECRET,
+});
 
 mqtt.listen(mqttPort, function() {
   console.log("mqtt server listening on port", mqttPort);
@@ -47,3 +56,44 @@ aedes.on("client", function(client) {
 app.get("/", (req, res) => {
   res.send('server is working!');
 });
+
+let previousFavoriteCount = 0;
+let newFavoriteCount = 0;
+
+const getLikeCount = async () => {
+  let response;
+  try {
+    response = await twitterClient.tweets.search({
+      q: 'from:stephaniecodes',
+      result_type: 'recent',
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  const recentTweets = response.statuses;
+  // console.log(recentTweets);
+  console.log('newFavoriteCount: ', newFavoriteCount);
+
+  const tweet = recentTweets.find(x => x.id === 1450787932785291300);
+  console.log('tweet favorite count test: ', tweet.favorite_count);
+
+  newFavoriteCount = recentTweets.reduce(
+    (acc, curr) => acc + curr.favorite_count,
+    0
+  );
+
+  console.log('previousFavoriteCount: ', previousFavoriteCount);
+  console.log('newFavoriteCount: ', newFavoriteCount);
+
+  if (previousFavoriteCount !== 0 && previousFavoriteCount < newFavoriteCount) {
+    console.log('🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳');
+  }
+
+  previousFavoriteCount = newFavoriteCount;
+};
+
+// start everything
+getLikeCount();
+setInterval(() => getLikeCount(), 6000);
+
