e0a6abd3aa7bd504a09d0fbecd3e2860a1934dd7
Stephanie Nemeth <stephanie@hey.com>
2021-10-21 19:20:59 +0200

    add aedes.publish call on new fave

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-require('dotenv').config()
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const path = require("path");
-const aedes = require("aedes")();
-const mqtt = require("net").createServer(aedes.handle);
-const httpServer = require("http").createServer(app);
-const ws = require("websocket-stream");
+const aedes = require('aedes')();
+const mqtt = require('net').createServer(aedes.handle);
+const httpServer = require('http').createServer(app);
+const ws = require('websocket-stream');
 const mqttPort = 1883;
 const appPort = 8080;
 
 const Twitter = require('twitter-lite');
 
-mqtt.listen(mqttPort, function() {
-  console.log("mqtt server listening on port", mqttPort);
+mqtt.listen(mqttPort, function () {
+  console.log('mqtt server listening on port', mqttPort);
 });
 
 ws.createServer(
   {
-    server: httpServer
+    server: httpServer,
   },
   aedes.handle
 );
 
-httpServer.listen(appPort, function() {
-  console.log("server listening on port", appPort);
+httpServer.listen(appPort, function () {
+  console.log('server listening on port', appPort);
 });
 
-aedes.on("clientError", function(client, err) {
-  console.log("client error", client.id, err.message, err.stack);
+aedes.on('clientError', function (client, err) {
+  console.log('client error', client.id, err.message, err.stack);
 });
 
-aedes.on("publish", function(packet, client) {
+aedes.on('publish', function (packet, client) {
+  console.log(packet);
   if (client) {
-    console.log("message from client", client.id);
+    console.log('message from client', client.id);
   }
 });
 
-aedes.on("subscribe", function(subscriptions, client) {
+aedes.on('subscribe', function (subscriptions, client) {
   if (client) {
-    console.log("subscribe from client", subscriptions, client.id);
+    console.log('subscribe from client', subscriptions, client.id);
   }
 });
 
-aedes.on("client", function(client) {
-  console.log("new client", client.id);
+aedes.on('client', function (client) {
+  console.log('new client', client.id);
 });
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.send('server is working!');
 });
 
@@ -81,7 +81,8 @@ const getLikeCount = async () => {
   console.log('newFavoriteCount: ', newFavoriteCount);
 
   if (previousFavoriteCount !== 0 && previousFavoriteCount < newFavoriteCount) {
-    console.log('🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳');
+    console.log('!!!! publish wavy-gravy event !!!');
+    aedes.publish('wavy-gravy', true);
   }
 
   previousFavoriteCount = newFavoriteCount;
@@ -90,4 +91,3 @@ const getLikeCount = async () => {
 // start everything
 getLikeCount();
 setInterval(() => getLikeCount(), 6000);
-
